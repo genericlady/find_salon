7bf3dd063dcb44bd3553cc8031a3dac1a920f8f6
Lady Codington <myemailforcoding@gmail.com>
2016-03-05 13:04:06 -0500

    abstracted result model spec from find salon

--- spec/models/result_spec.rb
+++ spec/models/result_spec.rb
@@ -1,22 +1,52 @@
-decribe FindSalon::Result, :vcr do
+require 'spec_helper'
+
+describe FindSalon::Result, :vcr do
+  let(:result) { FindSalon::Result.new }
+
   context 'properties' do
-    it 'has a name' do
+    it 'has many attributes' do
+      result.name = "Thai New York Spa & Salon"
+      result.vicinity = "23-92 21st Street, Astoria"
+      result.rating = 4.6
+      result.place_id = "ChIJHck_2VpfwokRoXdg8srpPsQ"
+      expect(result).to have_attributes(name: "Thai New York Spa & Salon",
+                                        vicinity: "23-92 21st Street, Astoria",
+                                        rating: 4.6,
+                                        place_id: "ChIJHck_2VpfwokRoXdg8srpPsQ")
+    end
+  end
+
+  describe '#save' do
+    it 'saves the result instance into all results' do
+      result.save
+
+      expect(FindSalon::Result.all).to include(result)
+    end
+  end
+
+  describe '.all' do
+    it 'returns all my result instances' do
+      result.save
+
+      expect(FindSalon::Result.all).to include(result)
+    end
+  end
+
+  describe '.reset_all!' do
+    it 'resets all results' do
+      result.save
+
+      FindSalon::Result.reset_all!
+      expect(FindSalon::Result.all).to be_empty
+    end
+  end
+
+  describe '.load from FindSalon' do
+    it 'loads results from FindSalon' do
+      FindSalon::Result.reset_all!
+      FindSalon::Result.load_from_findsalon
+      expect(FindSalon::Result.all).to_not be_empty
 
     end
-
-    it 'has a url' do
-      
-    end
-
-    it 'has reviews' do
-
-    end
-
-    it 'has a rating' do
-
-    end
-
-    it 'has an address' do
-
-    end
+  end
 end
