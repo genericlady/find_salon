a935b2d766c6889b368e292cd75e630392b996e7
Lady Codington <myemailforcoding@gmail.com>
2016-03-05 13:25:32 -0500

    loading results @ 2:18:52

--- lib/find_salon/result.rb
+++ lib/find_salon/result.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class FindSalon::Result
   attr_accessor :name, :vicinity, :rating, :place_id
 
@@ -15,6 +17,9 @@ class FindSalon::Result
   end
 
   def self.load_from_findsalon
+    uri = URI.parse("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=40.7732,-73.926&radius=500&type=beauty_salon&key=#{ENV['GOOGLE_PLACES_KEY']}")
 
+    response = Net::HTTP.get_response(uri)
+    json = JSON.parse(response.body)
   end
 end
