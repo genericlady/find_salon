e83927f3b270c54e96af0f9877aa2aeb32ca5732
Lady Codington <myemailforcoding@gmail.com>
2016-03-05 13:03:30 -0500

    added attributes and some basic methods

--- lib/find_salon/result.rb
+++ lib/find_salon/result.rb
@@ -1,2 +1,20 @@
 class FindSalon::Result
+  attr_accessor :name, :vicinity, :rating, :place_id
+
+  @@all = Array.new
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.reset_all!
+    @@all.clear
+  end
+
+  def self.load_from_findsalon
+
+  end
 end
